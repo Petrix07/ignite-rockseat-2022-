d14ba6a3a892c00fee3b065e9e2cebc92eecfe8f
Petrix <luizfpetris@gmail.com>
2023-02-19 21:31:30 -0300

    Estilizando componentes

--- React/aulas/01-fundamentos-reactjs/src/App.jsx
+++ React/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from '../componentes/Header';
 import { Post } from './Post';
+
+import './style.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Luiz Fernando Petris"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam dolor suscipit facere nemo aliquam, cum, ullam esse provident ex ratione mollitia, impedit aperiam nam magni quod? Nemo, nulla a."
