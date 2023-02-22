d87ae5a0403909d1d8f2494f8b53c4c5c27a1e93
Petrix <luizfpetris@gmail.com>
2023-02-22 20:40:26 -0300

    Alterado o sidebar da page e add a lib phosphor

--- React/ignite-feed/src/componentes/Sidebar.jsx
+++ React/ignite-feed/src/componentes/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,12 +8,14 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1629654297299-c8506221ca97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/75099491?v=4" />
         <strong>Luiz Fernando</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href='#'>
+          <PencilLine size="20" />
           Editar seu perfil
         </a>
       </footer>
