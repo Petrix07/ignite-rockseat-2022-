bf7ca02d46d0f80ba9ce5e91582ad04e390419de
Petrix <luizfpetris@gmail.com>
2023-02-21 21:28:48 -0300

    Inicio da implementação do Sidebar de perifl

--- React/ignite-feed/src/App.jsx
+++ React/ignite-feed/src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './componentes/Header';
+import { Sidebar } from './componentes/Sidebar';
 import { Post } from './Post';
+
+import styles from './componentes/App.module.css';
 
 import './global.css';
 
@@ -7,15 +10,19 @@ export function App() {
   return (
     <div>
       <Header />
-
-      <Post
-        author="Luiz Fernando Petris"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam dolor suscipit facere nemo aliquam, cum, ullam esse provident ex ratione mollitia, impedit aperiam nam magni quod? Nemo, nulla a."
-      />
-      <Post
-        author="Teste Autor"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam dolor suscipit facere nemo aliquam, cum, ullam esse provident ex ratione mollitia, impedit aperiam nam magni quod? Nemo, nulla a."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Luiz Fernando Petris"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam dolor suscipit facere nemo aliquam, cum, ullam esse provident ex ratione mollitia, impedit aperiam nam magni quod? Nemo, nulla a."
+          />
+          <Post
+            author="Teste Autor"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam dolor suscipit facere nemo aliquam, cum, ullam esse provident ex ratione mollitia, impedit aperiam nam magni quod? Nemo, nulla a."
+          />
+        </main>
+      </div>
     </div>
   )
 }
